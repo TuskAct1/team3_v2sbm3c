7fd5cfef12b07ec0a9782d4040b5c0e926afa2d0
An1013 <a8060a90@gmail.com>
2025-06-13 14:51:57 +0900

    feat: VO 업데이트

---
+++ src/main/java/dev/mvc/announcement/AnnounceDAOInter.java
@@ -0,0 +1,21 @@
+package dev.mvc.announcement;
+
+import java.util.List;
+
+public interface AnnounceDAOInter {
+  
+  /** 공지사항 등록 */
+  int create(AnnounceVO announceVO);
+  
+  /** 전체 공지사항 목록 */
+  List<AnnounceVO> list_all();
+  
+  /** 특정 공지사항 조회 */
+  AnnounceVO read(int announcementno);
+  
+  /** 공지사항 수정 */
+  int update(AnnounceVO announceVO);
+  
+  /** 공지사항 삭제 */
+  int delete(int announcementno);
+}
