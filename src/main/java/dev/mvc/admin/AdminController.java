27af69d9e60519cf85249dd13c154a8efd6111d3
oreadlee <leesh001122@naver.com>
2025-06-17 17:54:51 +0900

    “텍스트”

--- src/main/java/dev/mvc/admin/AdminController.java
+++ src/main/java/dev/mvc/admin/AdminController.java
@@ -1,4 +1,21 @@
 package dev.mvc.admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping("/admin")
+@Controller
 public class AdminController {
+  
+  @GetMapping("/create")
+  public String create() {
+    return "admin/create";
+  }
+  
+  @GetMapping("/list")
+  public String list() {
+    return "admin/list";
+  }
+  
 }
