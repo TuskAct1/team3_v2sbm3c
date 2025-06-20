84f300cc5ecb0c57827699f80b8f881d791a96d6
TuskAck1 <free5549@naver.com>
2025-06-20 17:14:07 +0900

    refactor: backend, frontend add

---
+++ src/main/webapp/main/src/pages/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function MainPage() {
+  return (
+    <div style={{ padding: '2rem' }}>
+      <h1>토닥이에 오신 걸 환영합니다 🤗</h1>
+      <p>지친 하루, 토닥이와 함께 이야기해봐요.</p>
+    </div>
+  );
+}
+
+export default MainPage;
