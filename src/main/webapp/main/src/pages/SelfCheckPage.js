8cc24b4935148312b6eb0141d2bd87abf5444408
TuskAck1 <free5549@naver.com>
2025-06-20 17:14:07 +0900

    refactor: backend, frontend add

---
+++ src/main/webapp/main/src/pages/SelfCheckPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SelfCheckPage() {
+  return <h1>자가진단 페이지입니다.</h1>;
+}
+
+export default SelfCheckPage;
---
+++ src/main/webapp/main/src/pages/SignUpPage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function SignUpPage() {
+  return <h1>회원가입 페이지입니다.</h1>;
+}
+
+export default SignUpPage;
