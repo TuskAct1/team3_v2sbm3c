d20497bf547b4f15a9c653a7f1eac8b66aad0b8c
TuskAck1 <free5549@naver.com>
2025-06-26 01:21:23 +0900

    feat: calender, reply 미완성

--- src/main/java/dev/mvc/reply/ReplyProc.java
+++ src/main/java/dev/mvc/reply/ReplyProc.java
@@ -3,6 +3,7 @@ package dev.mvc.reply;
 import java.util.ArrayList;
 import java.util.List;
 
+import dev.mvc.tool.Tool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -11,29 +12,46 @@ import org.springframework.stereotype.Service;
 public class ReplyProc implements ReplyProcInter {
   @Autowired
   private ReplyDAOInter replyDAO;
-  
+
   @Override
   public int create(ReplyVO replyVO) {
     int cnt = this.replyDAO.create(replyVO);
     return cnt;
   }
-  
+
   @Override
   public ReplyVO read(int replyno) {
     ReplyVO replyVO =this.replyDAO.read(replyno);
     return replyVO;
   }
-  
+
+  @Override
+  public ArrayList<ReplyVO>list_by_boardno(int boardno) {
+    return replyDAO.list_by_boardno(boardno);
+  }
+
   @Override
   public int update(ReplyVO replyVO) {
     int cnt = this.replyDAO.update(replyVO);
     return cnt;
   }
- 
+
   @Override
   public int delete(int replyno) {
     int cnt = this.replyDAO.delete(replyno);
     return cnt;
   }
-  
+  @Override
+  public ArrayList<ReplyMemberVO> list_by_boardno_join(int boardno) {
+    ArrayList<ReplyMemberVO> list = replyDAO.list_by_boardno_join(boardno);
+    String content = "";
+    // 특수 문자 변경
+    for (ReplyMemberVO replyMemberVO : list) {
+      content = replyMemberVO.getContent();
+      content = Tool.convertChar(content);
+      replyMemberVO.setContent(content);
+    }
+    return list;
+  }
+
 }
